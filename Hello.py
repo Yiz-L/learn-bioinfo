39f254ea71707972c591332ee5efc9f2a1616f83
YizL <yizl20230901@outlook.com>
2026-06-05 23:06:07 +0800

    📝 更新第4章完成状态，记录学习困惑点

--- Hello.py
+++ Hello.py
@@ -1,3 +1,13 @@
-cytokines = ["IL-2", "IL-6", "TNF-α","IFN-γ"]
-for cytokine in cytokines:
-    print(cytokine) 
+samples = []
+samples.append({"id":"S001", "type":"血清", "volume":1.5, "date":"2024-06-01"})
+samples.append({"id":"S002", "type":"血浆", "volume":2.0, "date":"2024-06-01"})
+samples.append({"id":"S003", "type":"血清", "volume":0.8, "date":"2024-06-01"})
+for sample in samples:
+    print(sample["id"] + " | " 
+          + sample["type"] + " | " 
+          + str(sample["volume"]) + "mL | " 
+          + sample["date"])
+total_volume = 0
+for sample in samples:
+    total_volume = total_volume + sample["volume"]
+print("总体积：" + str(total_volume) + " mL")
