cf7a03b590b86c841ebe5254237306a59c840bb8
YizL <yizl20230901@outlook.com>
2026-06-05 20:47:36 +0800

    📝 更新第3章学习进度：完成if判断和for循环

--- Hello.py
+++ Hello.py
@@ -1,5 +1,3 @@
-# 成年人BMI计算
-height = float(input("请输入您的身高（米）："))
-weight = float(input("请输入您的体重（千克）："))
-BMI = weight / (height ** 2)
-print("您的BMI为:"+str(BMI))
+cytokines = ["IL-2", "IL-6", "TNF-α","IFN-γ"]
+for cytokine in cytokines:
+    print(cytokine) 
